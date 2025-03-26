6af83e95073b900c6164da6c039447c864f7acfa
dylz555444999 <dylz555444999@gmail.com>
2025-03-26 18:45:41 +0000

    Added player movment speed clipping needs fixed!!!

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 10f;
+
+    Rigidbody2D myRigidBody2D;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        run();
+    }
+
+    private void run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody2D.linearVelocity.y);
+        myRigidBody2D.linearVelocity = playerVelocity;
+    }
+}
+
