8abc590e10fd5bbce9cb53edb87e6c2033be95c3
DESKTOP-OL3BKMO\dylan <dylz555444999@gmail.com>
2025-07-08 18:43:08 +0100

    Added Movement and maybe a bit more but mainly movemant

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,20 +3,27 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 10f;
+
+    Rigidbody2D myRigidBody2D;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        myRigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
     }
 
     private void Run()
     {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody2D.linearVelocity.y);
+        myRigidBody2D.linearVelocity = playerVelocity;
     }
 }
