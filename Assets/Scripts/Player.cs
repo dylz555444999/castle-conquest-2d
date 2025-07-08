f95f947a034f2688ad0c16b7057f5c6dc29e2477
DESKTOP-OL3BKMO\dylan <dylz555444999@gmail.com>
2025-07-08 22:33:49 +0100

    Added sprite flipping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
+using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
-
+[Obsolete]
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
@@ -23,7 +24,17 @@ public class Player : MonoBehaviour
     {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
 
-        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody2D.linearVelocity.y);
-        myRigidBody2D.linearVelocity = playerVelocity;
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody2D.velocity.y);
+        myRigidBody2D.velocity = playerVelocity;
+        FlipSprite();
+    }
+    private void FlipSprite()
+    {
+        bool runninghorizontally = Mathf.Abs(myRigidBody2D.velocity.x) > Mathf.Epsilon;
+
+        if (runninghorizontally)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody2D.velocity.x), 1f);
+        }
     }
 }
