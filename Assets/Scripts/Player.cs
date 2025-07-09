476636de7f6f97ce5caa787b64d57f7d424f202f
DESKTOP-OL3BKMO\dylan <dylz555444999@gmail.com>
2025-07-09 16:05:36 +0100

    Added layer detection to prevent double jumping and added jumpin working oon wall jump fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,19 +5,38 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 15f;
 
     Rigidbody2D myRigidBody2D;
+    Animator myAnimator;
+    BoxCollider2D myBoxCollider2D;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         myRigidBody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBoxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        jump();
+    }
+
+    private void jump()
+    {
+        if(!myBoxCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        bool isJumping = CrossPlatformInputManager.GetButtonDown("Jump");
+
+        if(isJumping)
+        {
+            Vector2 jumpVelocity = new Vector2(myRigidBody2D.velocity.x, jumpSpeed);
+            myRigidBody2D.velocity = jumpVelocity;
+        }
     }
 
     private void Run()
@@ -26,8 +45,17 @@ public class Player : MonoBehaviour
 
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody2D.velocity.y);
         myRigidBody2D.velocity = playerVelocity;
+        
         FlipSprite();
+        ChangingToRunningState();
     }
+
+    private void ChangingToRunningState()
+    {
+        bool runninghorizontally = Mathf.Abs(myRigidBody2D.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", runninghorizontally);
+    }
+
     private void FlipSprite()
     {
         bool runninghorizontally = Mathf.Abs(myRigidBody2D.velocity.x) > Mathf.Epsilon;
